a6ede7572d59032ffac18b52a0a65a5394243966
Ilyneke <kharisov.dmitry101@gmail.com>
2022-12-29 03:45:08 +0500

    You can add point now

--- main_app/static/main_app/js/main.js
+++ main_app/static/main_app/js/main.js
@@ -1,12 +1,16 @@
 const OL = window.ol
 
+
+
 let draw;
 const source = new OL.source.Vector({wrapX: false});
 const vector = new OL.layer.Vector({
     source: source,
   });
-
-
+const view = new OL.View({
+    center: [6747055, 7729412],
+    zoom: 10,
+  })
 const map = new OL.Map({
   target: 'map',
   layers: [
@@ -15,10 +19,7 @@ const map = new OL.Map({
     }),
     vector
   ],
-  view: new OL.View({
-    center: [6747055.141706806, 7729412.378274312],
-    zoom: 9,
-  }),
+  view: view
 });
 
 map.on('singleclick', function (evt) {
@@ -39,5 +40,11 @@ function addInteraction() {
 addInteraction();
 
 draw.on("drawend", (e) => {
+    let coord = e.feature.getGeometry().getCoordinates();
+    view.animate({
+        center: coord,
+        duration: 900,
+        zoom: 15,
+    })
     map.removeInteraction(draw);
 })
