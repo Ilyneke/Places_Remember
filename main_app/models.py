b932a945a43e63baf4852cad88f92a21b9b090b6
Ilyneke <kharisov.dmitry101@gmail.com>
2022-12-29 03:48:28 +0500

    First model created

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# class Map(models.Model):
-#     title = models.CharField(max_length=50)
-#     commentary = models.CharField(max_length=500)
-#     place = models.FloatField()
+class Memory(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    center0 = models.FloatField()
+    center1 = models.FloatField()
+    zoom = models.FloatField()
+
+    def __str__(self):
+        return self.title
