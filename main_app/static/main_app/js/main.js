5fcd9bd86cf84ed5a578879de948751eeb1609ff
Ilyneke <kharisov.dmitry101@gmail.com>
2022-12-29 01:37:40 +0500

    Start of map developing

--- main_app/static/main_app/js/main.js
+++ main_app/static/main_app/js/main.js
@@ -1,3 +1,43 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
+const OL = window.ol
+
+let draw;
+const source = new OL.source.Vector({wrapX: false});
+const vector = new OL.layer.Vector({
+    source: source,
+  });
+
+
+const map = new OL.Map({
+  target: 'map',
+  layers: [
+    new OL.layer.Tile({
+      source: new OL.source.OSM(),
+    }),
+    vector
+  ],
+  view: new OL.View({
+    center: [6747055.141706806, 7729412.378274312],
+    zoom: 9,
+  }),
+});
+
+map.on('singleclick', function (evt) {
+    // на время.
+    const coordinate = evt.coordinate;
+    console.log(coordinate)
+
+});
+
+
+function addInteraction() {
+    draw = new OL.interaction.Draw({
+      source: source,
+      type: "Point",
+    });
+    map.addInteraction(draw);
+}
+addInteraction();
+
+draw.on("drawend", (e) => {
+    map.removeInteraction(draw);
 })
