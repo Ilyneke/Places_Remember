d06298b5b07ec867328afb370c72f868e136cab5
Ilyneke <kharisov.dmitry101@gmail.com>
2022-12-29 01:42:31 +0500

    Scripts connects refactoring

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('create', views.addmem, name='addmemory')
-    # path('mypage', views.my_page),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
+from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 # from .forms import MapModelForm
 # from .models import Map
-
-
+#
 # class MapCreateView:
 #     template_name = 'main_app/addmemory.html'
 #     form_class = MapModelForm
 #     success_message = 'Success: memory was created.'
 #     success_url = reverse_lazy('main_app/index')
+
+
+class MarkersMapView(TemplateView):
+    """Markers map view."""
+
+    template_name = "map.html"
+
+
 def addmem(request):
     return render(request, 'main_app/addmemory.html')
 
