2fbb00601e3b6d8813bdf591cec7c86fdb6e02f5
Ilyneke <kharisov.dmitry101@gmail.com>
2023-01-02 22:11:28 +0500

    Add memory capability was added

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import logout
+from django.contrib.auth import logout, get_user
 from django.contrib.auth.models import AnonymousUser
 from .models import Memory
 from .forms import MemoryForm
@@ -9,16 +9,37 @@ def addmem(request):
     return render(request, 'main_app/addmemory.html')
 
 
+def view_map(request):
+    return render(request, 'main_app/view_map.html')
+
+
 def index(request):
-    form = MemoryForm()
+    form_error = ''
+    if request.method == 'POST':
+        form = MemoryForm(request.POST)
+        form['author'].initial = get_user(request).pk
+        print('form.data:', form.data)
+        print('\nform.is_valid():', form.is_valid())
+        if form.is_valid():
+            form.save()
+            form.clean()
+        else:
+            print('\nform.errors:', form.errors)
+            form_error = 'Форма была заполнена неправильно!'
+    else:
+        form = MemoryForm()
+    data = {
+        'error': form_error,
+        'form': form
+    }
     if not isinstance(request.user, AnonymousUser):  # если пользователь авторизован, возвращаем его воспоминания
         posts = Memory.objects.filter(author=request.user)
-        return render(request, 'main_app/index.html', {'posts': posts, 'form': form})
-    return render(request, 'main_app/index.html', {'form': form})  # иначе возвращаем без воспоминаний
+        data['posts'] = posts
+        return render(request, 'main_app/index.html', data)
+    return render(request, 'main_app/index.html', data)  # иначе возвращаем без воспоминаний
 
 
 def logout_view(request):
     logout(request)
-
 
 
